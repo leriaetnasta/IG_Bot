4909b716901e262952422ac3a2ff1fa43c7bd5bd
TALHA_LOUBNA <talha_loubna@emsi-edu.ma>
2022-07-14 19:23:27 +0100

    initial commit

---
+++ Multiple_DMs.py
@@ -0,0 +1,7 @@
+from instabot import Bot
+mybot=Bot()
+mybot.login(username="",password="")
+
+for  i in range(10):
+    mybot.send_message("up", [""])
+
---
+++ insta_DM_Bot.py
@@ -0,0 +1,4 @@
+from instabot import Bot
+mybot=Bot()
+mybot.login(username="",password="")
+mybot.send_message("up",[""])
